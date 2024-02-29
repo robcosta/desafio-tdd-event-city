975632529cd5b140b2161e10b14c4de3b01eb90b
Roberto Costa <robertocosta66@gmail.com>
2024-02-29 15:34:26 -0300

    CityControllerIT - findAllShouldReturnAllResourcesSortedByName

---
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -0,0 +1,5 @@
+package com.devsuperior.demo.controllers;
+
+public class EventController {
+
+}
---
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.repositories.CityRepository;
+
+
+
+@Service
+public class CityService {
+
+	@Autowired
+	private CityRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<CityDTO> findAll(){
+		List<City> result = repository.findAll(Sort.by("name"));
+		return result.stream().map(x -> new CityDTO(x)).toList();
+	}
+}
